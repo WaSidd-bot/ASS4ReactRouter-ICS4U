d0db2fc04e06f6453c97f8042c63bc646ca825aa
Wasay Siddiqui <wasaysiddiqui379@gmail.com>
2026-04-22 10:10:28 -0400

    baseplate code to github repository for editing

---
+++ ReactRouter/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+import path from 'path';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    babel({ presets: [reactCompilerPreset()] })
+  ],
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // Set "@" to refer to the root folder
+    },
+  },
+})
